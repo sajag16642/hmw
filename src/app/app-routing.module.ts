63a47e8e936192e90c206261cad1ed118fae94fc
sajag16642 <sajag16642gmail.com>
2019-09-06 18:00:22 +0530

    blank

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './LandingPage/home.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditProfileComponent } from '../../src/app/edit-profile/edit-profile.component';
+import { ViewCentresComponent } from './view-centres/view-centres.component';
+import { GaurdService } from './Services/gaurd.service';
+import { ContactedUserProfileComponent } from './contacted-user-profile/contacted-user-profile.component';
+import { OnboardingComponent } from './onboarding/onboarding.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full',component: HomeComponent },
+  { path: 'home',redirectTo: '',  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'subscription', component: SubscriptionComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate:[GaurdService]},
+  { path: 'EditProfile', component: EditProfileComponent, canActivate:[GaurdService]},
+  { path: 'viewCentres', component: ViewCentresComponent},
+  { path: 'contactedUserProfiles/:userId/:status', component:ContactedUserProfileComponent},
+  { path: 'onboarding', component: OnboardingComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes , {
+    scrollPositionRestoration: 'enabled'
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
